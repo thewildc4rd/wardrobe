fdf3c940dc7fd12905ea27ec8ed8501a18f600d4
Sabrina Nahabedian <sabrina.nahabedian@gmail.com>
2025-05-10 22:56:16 +1000

    edit user page

--- wardrobe-app/app/profile/page.js
+++ wardrobe-app/app/profile/page.js
@@ -15,17 +15,22 @@ export default function ProfilePage({}) {
 	}, []);
 
 	return (
-		<div className='h-full flex flex-col bg-white-pink'>
-			<div className='h-36 bg-pink-medium flex justify-center py-2'>
-				<div className='w-[50%] flex items-end'>
-					<div className='h-36 w-36 overflow-hidden rounded-full items-end bg-white-pink flex justify-center relative top-12 border-8 border-brown-darkest'>
-						<UserIcon width={'120'} height={'120'} />
-					</div>
-					<div className='flex flex-col'>
-						<h1 className='ml-6 text-4xl font-bold text-brown-darkest'>Sabrina</h1>
-						<h1 className='ml-6 text-3xl font-medium text-brown-darkest'>Nahabedian</h1>
+		<div className='h-full flex flex-col items-center bg-white-pink'>
+			<div className='w-full h-45 bg-brown-medium flex justify-center py-2'>
+				<div className='w-[75%] flex items-end'>
+					<div className='h-50 w-50 overflow-hidden rounded-full items-end bg-white-pink flex justify-center relative top-15 border-8 border-brown-darkest'>
+						<UserIcon width={'175'} height={'175'} />
 					</div>
 				</div>
+			</div>
+			<div className='mt-20 w-[80%] flex flex-row'>
+				<div className='flex-1 flex flex-col'>
+					<div className='flex flex-col gap-2'>
+						<h1 className='ml-6 text-5xl font-semibold text-brown-darkest'>Sabrina</h1>
+						<h1 className='ml-6 text-5xl font-semibold text-brown-darkest'>Nahabedian</h1>
+					</div>
+				</div>
+				<div className='flex-1'></div>
 			</div>
 		</div>
 	);
